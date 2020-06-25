46d1a057dfd3f34cdca8eee9b020752585dac0da
solomon911 <solomonsahayaraj911@gmail.com>
2020-06-25 21:03:16 +0530

    1 file modified

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
 
 const authProvider = 'cognito-idp.us-west-2.amazonaws.com/us-west-2_randomtext,cognito-idp.us-west-2.amazonaws.com/us-west-2_randomtext:CognitoSignIn:123wd-32fa-hfgg-9a2d-sdfssd123';
-        const parts = authProvider.split(':');
-        const userPoolIdParts = parts[parts.length - 3].split('/');
-        const userPoolId = userPoolIdParts[userPoolIdParts.length - 1];
-        const userPoolUserId = parts[parts.length - 1]; // == userid
-        console.log("Cognito User: ", userPoolUserId);
-        appDiv.innerHTML = `<h1>${userPoolUserId}</h1>`;
+const parts = authProvider.split(':');
+const userPoolIdParts = parts[parts.length - 3].split('/');
+const userPoolId = userPoolIdParts[userPoolIdParts.length - 1];
+const userPoolUserId = parts[parts.length - 1]; // == userid
+console.log("Cognito User: ", userPoolUserId);
+appDiv.innerHTML = `<h1>${userPoolUserId}</h1>`;
