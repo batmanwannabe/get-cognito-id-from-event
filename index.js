bf032a88f6261156b0d407c94086364ccf0bbfb2
solomon911 <solomonsahayaraj911@gmail.com>
2020-06-25 12:00:56 +0530

    1 file modified

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import './style.css';
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
 
-const authProvider = 'cognito-idp.us-west-2.amazonaws.com/us-west-2_VJehLel7Y,cognito-idp.us-west-2.amazonaws.com/us-west-2_VJehLel7Y:CognitoSignIn:a5e3bda5-efff-4aa1-9a2d-1299f46d29ad';
+const authProvider = 'cognito-idp.us-west-2.amazonaws.com/us-west-2_randomtext,cognito-idp.us-west-2.amazonaws.com/us-west-2_randomtext:CognitoSignIn:123wd-32fa-hfgg-9a2d-sdfssd123';
         const parts = authProvider.split(':');
         const userPoolIdParts = parts[parts.length - 3].split('/');
         const userPoolId = userPoolIdParts[userPoolIdParts.length - 1];
